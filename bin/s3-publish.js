f6df35d8f7348985bed05e0e390353b753c1af80
Mike North <northm@yahoo-inc.com>
2014-10-19 01:09:30 -0700

    ember publisher wildcard from master branch

--- bin/s3-publish.js
+++ bin/s3-publish.js
@@ -4,4 +4,7 @@ var S3Publisher = require('ember-publisher');
 var configPath = require('path').join(__dirname, './s3-config.js');
 
 var publisher = new S3Publisher({projectConfigPath: configPath});
+publisher.currentBranch = function() {
+  return (process.env.TRAVIS_BRANCH === 'master') ? 'wildcard' : 'no-op';
+};
 publisher.publish();
