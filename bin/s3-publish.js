dc799f9b50746d1cc55e804ea08bfcde54da9de7
Mike North <northm@yahoo-inc.com>
2014-10-19 01:35:43 -0700

    update ember-publish stream selection logic

--- bin/s3-publish.js
+++ bin/s3-publish.js
@@ -5,6 +5,10 @@ var configPath = require('path').join(__dirname, './s3-config.js');
 
 var publisher = new S3Publisher({projectConfigPath: configPath});
 publisher.currentBranch = function() {
-  return (process.env.TRAVIS_BRANCH === 'master') ? 'wildcard' : 'release';
+  return (process.env.TRAVIS_BRANCH === 'master') ?
+    'wildcard' :
+    /v(?:[0-9]\.){2}[0-9](?:[A-Za-z\-]*[\.0-9]*)/.test(process.env.TRAVIS_BRANCH) ?
+      'release' :
+      'no-op';
 };
 publisher.publish();
